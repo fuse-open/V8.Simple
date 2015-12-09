a8ced35c0b08a0a8d0c77f123c64596012acb044
Olle Fredriksson <olle@fusetools.com>
2015-12-09 10:27:36 +0100

    V8: Use libc++ on OSX

--- osx_build.sh
+++ osx_build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export LDFLAGS=" -Ldeps/libs/osx -stdlib=libstdc++ -lv8_base -lv8_libbase -lv8_libplatform -lv8_nosnapshot"
-export CXXFLAGS=" -Ideps -m32 -stdlib=libstdc++"
+export LDFLAGS=" -Ldeps/libs/osx -stdlib=libc++ -lv8_base -lv8_libbase -lv8_libplatform -lv8_nosnapshot"
+export CXXFLAGS=" -Ideps -m32 -stdlib=libc++"
 export OBJ_DIR="obj/osx"
 make
