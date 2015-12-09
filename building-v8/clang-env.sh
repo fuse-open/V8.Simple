8cdfa5dc2315fff15a7410298f343ca096d965cb
Olle Fredriksson <olle@fusetools.com>
2015-12-09 10:25:31 +0100

    V8: Add some build notes

---
+++ building-v8/clang-env.sh
@@ -0,0 +1,9 @@
+export CXX=`which clang++`
+export CC=`which clang`
+export CPP="`which clang` -E -std=c++11 -stdlib=libc++"
+export LINK="`which clang++` -std=c++11 -stdlib=libc++"
+export CXX_host=`which clang++`
+export CC_host=`which clang`
+export CPP_host="`which clang` -E"
+export LINK_host=`which clang++`
+export GYP_DEFINES="clang=1 mac_deployment_target=10.9"
