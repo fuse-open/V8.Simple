e4615a4a193ad3c05d949aa9434a5fe08d84eaf3
Olle Fredriksson <olle@fusetools.com>
2015-12-10 11:19:24 +0100

    Build fat library on OSX

--- osx_build.sh
+++ osx_build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export LDFLAGS=" -Ldeps/libs/osx -stdlib=libstdc++ -lv8_base -lv8_libbase -lv8_libplatform -lv8_nosnapshot"
-export CXXFLAGS=" -Ideps -m32 -stdlib=libstdc++"
+export CXXFLAGS=" -Ideps -arch i386 -arch x86_64 -stdlib=libstdc++"
 export OBJ_DIR="obj/osx"
 make
