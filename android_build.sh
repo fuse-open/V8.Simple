cbb0e271f0da4776ae667cb816bbbf4296f004ee
Olle Fredriksson <olle@fusetools.com>
2016-01-08 16:18:19 +0100

    Optimise for size

--- android_build.sh
+++ android_build.sh
@@ -7,5 +7,5 @@ export OBJ_DIR="obj/android"
 export CXX=`find "$PREBUILT_DIR" -name arm-linux-androideabi-g++`
 COMMON_FLAGS="--sysroot=$SYSROOT_DIR -isystem $STLPORT_DIR/stlport -isystem $SYSROOT_DIR/usr/include"
 export LDFLAGS=" -Ldeps/libs/android $COMMON_FLAGS -fdata-sections -ffunction-sections -Wl,--whole-archive $STLPORT_DIR/libs/armeabi-v7a/libstlport_static.a -lv8_base -lv8_libbase -lv8_libplatform -lv8_nosnapshot -Wl,--no-whole-archive -Wl,--gc-sections -Wl,--strip-all"
-export CXXFLAGS=" -Ideps $COMMON_FLAGS"
+export CXXFLAGS=" -Os -Ideps $COMMON_FLAGS"
 make lib/libV8Simple.so
--- osx_build.sh
+++ osx_build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export LDFLAGS=" -Ldeps/libs/osx -stdlib=libstdc++ -lv8_base -lv8_libbase -lv8_libplatform -lv8_nosnapshot"
-export CXXFLAGS=" -Ideps -arch i386 -arch x86_64 -stdlib=libstdc++"
+export CXXFLAGS=" -Os -Ideps -arch i386 -arch x86_64 -stdlib=libstdc++"
 export OBJ_DIR="obj/osx"
 make lib/V8Simple.net.dll lib/libV8Simple.dylib
 make check
