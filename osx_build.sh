270dd352cd09c3cca575c236a0ba6facea23cecc
Olle Fredriksson <olle@fusetools.com>
2016-01-21 11:15:28 +0100

    Set install_name on OS X

--- osx_build.sh
+++ osx_build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export LDFLAGS=" -Ldeps/libs/osx -stdlib=libstdc++ -lv8_base -lv8_libbase -lv8_libplatform -lv8_nosnapshot"
+export LDFLAGS=" -install_name @rpath/libV8Simple.dylib -Ldeps/libs/osx -stdlib=libstdc++ -lv8_base -lv8_libbase -lv8_libplatform -lv8_nosnapshot"
 export CXXFLAGS=" -Os -Ideps -arch i386 -arch x86_64 -stdlib=libstdc++"
 export OBJ_DIR="obj/osx"
 make lib/V8Simple.net.dll lib/libV8Simple.dylib
