ef193cb0fa46fbf42137561855535347e6c08819
Olle Fredriksson <olle@fusetools.com>
2016-02-16 15:39:15 +0100

    Set soname in android lib

--- android_build.sh
+++ android_build.sh
@@ -6,6 +6,6 @@ STLPORT_DIR="$NDK_DIR/sources/cxx-stl/stlport"
 export OBJ_DIR="obj/android"
 export CXX=`find "$PREBUILT_DIR" -name arm-linux-androideabi-g++`
 COMMON_FLAGS="--sysroot=$SYSROOT_DIR -isystem $STLPORT_DIR/stlport -isystem $SYSROOT_DIR/usr/include"
-export LDFLAGS=" -Ldeps/libs/android $COMMON_FLAGS -fdata-sections -ffunction-sections -Wl,--whole-archive $STLPORT_DIR/libs/armeabi-v7a/libstlport_static.a -lv8_base -lv8_libbase -lv8_libplatform -lv8_nosnapshot -Wl,--no-whole-archive -Wl,--gc-sections -Wl,--strip-all"
+export LDFLAGS=" -Ldeps/libs/android $COMMON_FLAGS -fdata-sections -ffunction-sections -Wl,--whole-archive $STLPORT_DIR/libs/armeabi-v7a/libstlport_static.a -lv8_base -lv8_libbase -lv8_libplatform -lv8_nosnapshot -Wl,--no-whole-archive -Wl,--gc-sections -Wl,--strip-all -Wl,-soname,libV8Simple.so"
 export CXXFLAGS=" -Os -Ideps $COMMON_FLAGS"
 make lib/libV8Simple.so
