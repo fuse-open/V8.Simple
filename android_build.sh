08e3cf4d1ce1d375c012230b5b077c6b8dbb96a8
Olle Fredriksson <olle@fusetools.com>
2016-12-05 15:40:15 +0100

    android build amend

--- android_build.sh
+++ android_build.sh
@@ -12,7 +12,7 @@ SUPPORT_INCLUDE_DIR="$NDK_DIR/sources/android/support/include"
 export OBJ_DIR="obj/android"
 export CXX=`find "$PREBUILT_DIR" -name arm-linux-androideabi-g++`
 COMMON_FLAGS="--sysroot=$SYSROOT_DIR -isystem $STDLIB_INCLUDE_DIR -isystem $SYSROOT_DIR/usr/include -I$SUPPORT_INCLUDE_DIR"
-export LDFLAGS=" -L$SYSROOT_DIR/usr/lib -Ldeps/libs/android $COMMON_FLAGS -Wl,--whole-archive -lv8_base -lv8_libbase -lv8_libplatform -lv8_libsampler -lv8_nosnapshot -L$STDLIB_LIB_DIR -landroid_support -lunwind -l$STDLIB -l$STDLIBABI -Wl,--no-whole-archive -Wl,--gc-sections -Wl,-soname,libV8Simple.so"
+export LDFLAGS=" -L$SYSROOT_DIR/usr/lib -Ldeps/libs/android $COMMON_FLAGS -Wl,--whole-archive -lv8_base -lv8_libbase -lv8_libplatform -lv8_libsampler -lv8_nosnapshot -L$STDLIB_LIB_DIR -landroid_support -lunwind -llog -l$STDLIB -l$STDLIBABI -Wl,--no-whole-archive -Wl,--gc-sections -Wl,-soname,libV8Simple.so -Wl,--strip-all"
 # export LDFLAGS=" -Ldeps/libs/android $COMMON_FLAGS -fdata-sections -ffunction-sections -Wl,--whole-archive -lv8_base -lv8_libbase -lv8_libplatform -lv8_libsampler -lv8_nosnapshot -L$STDLIB_LIB_DIR -landroid_support -lunwind -l$STDLIB -Wl,--no-whole-archive -Wl,--gc-sections -Wl,-soname,libV8Simple.so -Wl,--strip-all"
 export CXXFLAGS=" -shared -Os -Ideps $COMMON_FLAGS -fvisibility=hidden -fvisibility-inlines-hidden -DBUILDING_DLL -fdata-sections -ffunction-sections"
 make lib/libV8Simple.so
