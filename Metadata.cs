7c3497fe433e4079db48feed27f71eeb79e54033
Morten Daniel Fornes <mdfornes@gmail.com>
2023-09-10 20:53:16 +0700

    Upgrade to .NET 6.0
    This adds csproj-files for building using .NET 6.0 and adds assembly
    attributes required for loading the native library on macOS (Uno 3.x).
    
    Run `make lib/V8Simple.net.dll` to build the new assembly.

---
+++ Metadata.cs
@@ -0,0 +1,5 @@
+using System.Runtime.InteropServices;
+using System.Reflection;
+
+[assembly: DefaultDllImportSearchPaths(DllImportSearchPath.AssemblyDirectory)]
+[assembly: AssemblyMetadata("Uno.DllImportResolver", "True")]
